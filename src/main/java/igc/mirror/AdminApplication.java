2dc7e9b61af8acb461e61dd1a9cb9c31cd37cda5
Стронин <stronini@inlinegroup-c.ru>
2023-04-05 11:09:48 +0300

    security: remove logging in main

--- src/main/java/igc/mirror/AdminApplication.java
+++ src/main/java/igc/mirror/AdminApplication.java
@@ -9,15 +9,12 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class AdminApplication extends SpringBootServletInitializer {
-    static final Logger logger = LoggerFactory.getLogger(AdminApplication.class);
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        logger.info("Run from servlet");
         return super.configure(builder);
     }
 
     public static void main(String[] args) {
-        logger.info("Run from main");
         SpringApplication.run(AdminApplication.class, args);
     }
 }

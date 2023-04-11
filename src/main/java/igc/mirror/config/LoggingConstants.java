d5f2200276d1e88ab91f8e59223078aa2d86817a
tihomirovg <german.tihomirov@inlinegroup-c.ru>
2023-04-11 15:56:28 +0300

    #6

---
+++ src/main/java/igc/mirror/config/LoggingConstants.java
@@ -0,0 +1,7 @@
+package igc.mirror.config;
+
+public class LoggingConstants {
+    public static final String USER_AGENT_KEY = "http_user_agent";
+    public static final String X_REQUEST_ID_KEY = "http_request_id";
+    public static final String X_REQUEST_ID_HEADER = "X-Request-ID";
+}

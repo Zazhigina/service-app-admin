deb991f6a3803d5b7d90e0d99d901767566e8ca4
filimonovs <sergey.filimonov@inlinegroup-c.ru>
2023-07-11 18:41:03 +0300

    insert into t_t_variable #115

---
+++ src/main/resources/db/migration/V230711183147__insert_into_t_variable.sql
@@ -0,0 +1,22 @@
+/*
+
+Разработка-198-Загрузка данных для таблицы admi.t_variable
+https://gitlab.inlinegroup-c.ru/mirror/model/-/issues/115
+
+*/
+
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1011002, '{Наименование_поставщика}', 'Наименование поставщика', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1012002, '{Наименование_компании}', 'Наименование компании', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1013002, '{Предмет_закупки}', 'Предмет закупки', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1014002, '{Срок_ответа}', 'Срок ответа', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1015002, '{Ссылка_поставщика}', 'Индивидуальная ссылка поставщика', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1016002, '{ФИО_инициатора}', 'ФИО инициатора', 'Иванов Иван Иванович', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1017002, '{Имя_отчество_инициатора}', 'Имя и отчество инициатора', 'Иван Иванович', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1018002, '{Должность_инициатора}', 'Должность инициатора', 'Ведущий специалист', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1019002, '{Электронная_почта_инициатора}', 'Электронная почта инициатора', 'ivanov.ii@mail.ru', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1020002, '{Телефон_инициатора}', 'Телефон инициатора', '456-675-654', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1021002, '{Дата_время_отправки_ответов}', 'Дата и время отправки ответов', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1022002, '{Дата_время_отправки_запроса}', 'Дата и время отправки запроса поставщику', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1023002, '{Электронная_почта_поставщика}', 'Электронная почта поставщика', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1024002, '{Количество_поставщиков_предоставили_ответ}', 'Количество поставщиков, которые предоставили ответ', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1025002, '{Дата_время_подачи_предложения_поставщиком}', 'Дата и время подачи предложения поставщиком', '', '2023-07-11 17:51:28.587', NULL, NULL, NULL);

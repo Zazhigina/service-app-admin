991b85b1ac2a5e9a6a4f00c3c151464b48353acf
filimonovs <sergey.filimonov@inlinegroup-c.ru>
2023-07-25 13:38:53 +0300

    insert into a_param #92

---
+++ src/main/resources/db/migration/V230725132051__insert_into_a_param.sql
@@ -0,0 +1,8 @@
+/*
+
+Разработка-199-Сценарий 4. Заполнение поставщиком КП Общий профиль (БД)
+https://gitlab.inlinegroup-c.ru/mirror/model/-/issues/92#note_80562
+
+*/
+
+INSERT INTO "admin".a_param ("key", "name", val, create_date, create_user, last_update_date, last_update_user, default_val) VALUES('PARTNER_EXCHANGE.EMAIL_NOREPLY', 'Технический почтовый ящик для приема быстрых ответов от поставщиков', 'effect_zakupki@gazprom-neft.ru', '2023-07-25 13:32:22.862', NULL, NULL, NULL, NULL);

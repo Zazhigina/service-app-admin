602f1aa51371310aec6675bc2073a23f362a9dfb
filimonovs <sergey.filimonov@inlinegroup-c.ru>
2023-07-31 16:41:49 +0300

    insert data #92

---
+++ src/main/resources/db/migration/V230731163549__insert_data.sql
@@ -0,0 +1,9 @@
+/*
+
+1.
+Разработка-199-Сценарий 4. Заполнение поставщиком КП Общий профиль (БД)
+https://gitlab.inlinegroup-c.ru/mirror/model/-/issues/92#note_83677
+
+*/
+
+INSERT INTO "admin".t_variable (id, "name", description, default_val, create_date, create_user, last_update_date, last_update_user) VALUES(1060003, '{Обновленное_коммерческое_предложение}', 'Обновленное коммерческое предложение', NULL, '2023-07-31 16:39:00.398', NULL, NULL, NULL);

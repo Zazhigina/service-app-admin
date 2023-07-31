2f0bca07caaa04f1fddd3e46260d647837a43183
filimonovs <sergey.filimonov@inlinegroup-c.ru>
2023-07-31 11:13:39 +0300

    task-199 modify data t_letter_template #92

---
+++ src/main/resources/db/migration/V230731103742__modify_data_t_letter_template.sql
@@ -0,0 +1,8 @@
+/*
+
+Разработка-199-Сценарий 4. Заполнение поставщиком КП Общий профиль (БД)
+https://gitlab.inlinegroup-c.ru/mirror/model/-/issues/92#note_83436
+
+*/
+
+INSERT INTO "admin".t_letter_template (id, letter_type, title, letter_sample, create_date, create_user, last_update_date, last_update_user, type_template, acceptable_document_format, status) VALUES(1057003, 'EP_COMMERCIAL_OFFER_SENT', 'Effect. Анализ рынка - коммерческое предложение отправлено', NULL, '2023-07-31 10:28:05.291', NULL, NULL, NULL, 'TEMPLATE', NULL, 'DRAFT') ;

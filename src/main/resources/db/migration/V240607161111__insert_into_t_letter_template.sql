11bf47c0dd5dfe34b5ffb689283321525db37f7f
tihomirovg <german.tihomirov@inlinegroup-c.ru>
2024-06-07 16:21:48 +0300

    #113 DB: insert data into t_letter_template

---
+++ src/main/resources/db/migration/V240607161111__insert_into_t_letter_template.sql
@@ -0,0 +1,7 @@
+/*
+    Разработка-54-Реализация доп настроек ведения данных для справки в сервисе администрирования
+    https://gitlab.inlinegroup-c.ru/mirror/backend/service-app-admin/-/issues/113
+*/
+
+INSERT INTO t_letter_template (id,letter_type,title,letter_sample,create_date,type_template,acceptable_document_format,status) VALUES
+	 (281005,'EP_PARTNER_GUIDE_SIMPLE','Инструкция Поставщика "КП загрузкой файлом"',NULL,'2024-06-07 16:16:33.627073','DOCUMENT','pdf','DRAFT');

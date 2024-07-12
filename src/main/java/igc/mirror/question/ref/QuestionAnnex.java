9c055c419eafc0633a04d71a407227d22c209478
Зяблов Андрей <andrey.zyablov@inlinegroup-c.ru>
2024-07-12 12:45:13 +0300

    #115 Разработка-910-Добавление требований/файлов к каждому вопросу запроса заинтересованности.

---
+++ src/main/java/igc/mirror/question/ref/QuestionAnnex.java
@@ -0,0 +1,22 @@
+package igc.mirror.question.ref;
+
+public enum QuestionAnnex {
+    EMPTY("Нет требований (пусто)"),
+    FILE("Файл"),
+    COMMENT("Комментарий");
+
+    private final String name;
+
+    QuestionAnnex(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override // При сохранении в БД использовать только этот метод. (!) Не использовать метод .name()
+    public String toString() {
+        return this.equals(QuestionAnnex.EMPTY) ? null : super.toString();
+    }
+}

57b267cc2bfe00520dfeda24f9790089139a271f
Alexandr Reshtnikov <lexx2005@inbox.ru>
2024-09-06 23:12:35 +0500

    #118 accountExpires fix2

--- src/main/java/igc/mirror/ldapuser/service/LdapUserService.java
+++ src/main/java/igc/mirror/ldapuser/service/LdapUserService.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.core.AttributesMapper;
-import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.ldap.filter.AndFilter;
@@ -22,7 +21,6 @@ import javax.naming.directory.Attributes;
 import javax.naming.directory.SearchControls;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
@@ -99,7 +97,7 @@ public class LdapUserService {
 
                 var controls = new SearchControls();
                 controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
-                controls.setReturningAttributes(new String[] { "sAMAccountName", "cn", "description", "accountexpires", "useraccountcontrol", "pwdlastset" });
+                controls.setReturningAttributes(new String[] { "sAMAccountName", "cn", "description", "useraccountcontrol", "msDS-UserPasswordExpiryTimeComputed" });
 
                 AndFilter filter = new AndFilter();
                 filter.and(new EqualsFilter("objectclass", "person"));
@@ -114,21 +112,6 @@ public class LdapUserService {
 
                 var users = ldapTemplate.search("", filter.encode(), controls, new LdapUserAttributesMapper());
                 ldapUsers.addAll(users);
-
-                long maxPwdAge = 0;
-                DirContextAdapter cntx = (DirContextAdapter)ldapTemplate.lookup("");
-                if(cntx.attributeExists("maxpwdage")) {
-                    maxPwdAge = Math.abs(Long.parseLong((String)cntx.getObjectAttribute("maxpwdage")) / 10_000 / 1000);
-                }
-                for(LdapUser ldapUser : ldapUsers) {
-                    boolean isNoExpires = ldapUser.getIsNoExpires();
-                    if(!isNoExpires && maxPwdAge != 0 && ldapUser.getPwdLastSet() != null) {
-                        ldapUser.setAccountExpires(ldapUser.getPwdLastSet().plus(maxPwdAge, ChronoUnit.SECONDS));
-                    }
-                    if(isNoExpires) {
-                        ldapUser.setAccountExpires(LocalDateTime.MAX);
-                    }
-                }
 
             } catch (Exception e) {
                 logger.info("Ошибка при получении ролей СУЗ: {}", e.getMessage());
@@ -163,16 +146,12 @@ public class LdapUserService {
             if(attrs.get("description") != null) {
                 ldapUser.setDescription((String)attrs.get("description").get());
             }
-            if(attrs.get("accountexpires") != null) {
-                ldapUser.setAccountExpires(getDateFromAD(Long.parseLong((String)attrs.get("accountexpires").get())));
-            }
-            if(attrs.get("pwdlastset") != null) {
-                ldapUser.setPwdLastSet(getDateFromAD(Long.parseLong((String)attrs.get("pwdlastset").get())));
+            if(attrs.get("msds-userpasswordexpirytimecomputed") != null) {
+                ldapUser.setAccountExpires(getDateFromAD(Long.parseLong((String)attrs.get("msds-userpasswordexpirytimecomputed").get())));
             }
             if(attrs.get("useraccountcontrol") != null) {
                 String user_acc_ctrl = (String)attrs.get("useraccountcontrol").get();
                 ldapUser.setIsDisabled(user_acc_ctrl.equals("514") || user_acc_ctrl.equals("66050"));
-                ldapUser.setIsNoExpires(user_acc_ctrl.equals("66048") || user_acc_ctrl.equals("66050"));
             }
 
             List<LdapGroup> memberOf = new ArrayList<>();
